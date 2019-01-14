7d3e43fd8033f0c04ce9dc5bd4d0d2709eae8699
rishab26201 <39146400+rishab26201@users.noreply.github.com>
2019-01-14 11:55:43 +0530

    Update and rename voronoi_animate.py to voronoi/voronoi_animate.py

--- voronoi/voronoi_animate.py
+++ voronoi/voronoi_animate.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
+########    Making the voronoi diagram with scipy   ########
+
 # make up data points
 points = np.array([[282, 339], [67, 442], [462, 356], [370, 435], [185, 5]])
 
